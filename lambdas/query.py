d36cb7015d72eba7fa7794cef4298b254521e4a9
bcosta <brunosccosta@gmail.com>
2025-01-20 20:34:28 +0100

    query now returns something

--- lambdas/query.py
+++ lambdas/query.py
@@ -1,14 +1,19 @@
 import boto3
 import json
-from datetime import datetime, timedelta
+import logging
+from datetime import datetime, timezone, timedelta
 import os
+
+# Initialize the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Initialize DynamoDB table
 DYNAMO_TABLE_NAME = os.environ['DYNAMO_TABLE_NAME']
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(DYNAMO_TABLE_NAME)
 
-def query_dynamo(event, context):
+def lambda_handler(event, context):
     """
     Lambda handler for querying data from DynamoDB.
     """
@@ -25,12 +30,14 @@ def query_dynamo(event, context):
             }
 
         # Calculate the time range
-        end_time = datetime.now(datetime.timezone.utc)
+        end_time = datetime.now(timezone.utc)
         start_time = end_time - timedelta(hours=hours)
 
         # Format timestamps for DynamoDB
         start_time_str = start_time.strftime('%Y-%m-%d %H:%M:%S')
         end_time_str = end_time.strftime('%Y-%m-%d %H:%M:%S')
+
+        logger.info(f'Received query request: {device_id}@{hours}. Will query data between {start_time_str} and {end_time_str}.')
 
         # Query DynamoDB
         response = table.query(
@@ -48,11 +55,32 @@ def query_dynamo(event, context):
 
         # Return data sorted by timestamp
         items = response.get('Items', [])
-        items.sort(key=lambda x: x['timestamp'])
+        items.sort(key=lambda x: x['timestamp'], reverse=True)
 
+        # Convert the latest items to the desired format
+        formatted_items = []
+        for item in items:
+            formatted_item = {
+                'timestamp': item['timestamp'],
+                'angle': str(item['angle']),
+                'battery': str(item['battery']),
+                'gravity': str(item['gravity']),
+                'temperature': str(item['temperature'])
+            }
+            formatted_items.append(formatted_item)
+
+        # Add CORS headers to the response
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'OPTIONS,GET'
+        }
+
+        # Return a JSON response with the items and headers
         return {
-            "statusCode": 200,
-            "body": json.dumps(items)
+            'statusCode': 200,
+            'headers': headers,
+            'body': json.dumps(formatted_items)
         }
 
     except Exception as e:
